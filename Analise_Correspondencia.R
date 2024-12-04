80629a0327a6441e6cac2e225e647c3bacfb968a
Mário Diego Rocha Valente <83090653+MarioDhiego@users.noreply.github.com>
2024-12-04 15:27:59 -0300

    Add files via upload

--- Analise_Correspondencia.R
+++ Analise_Correspondencia.R
@@ -22,6 +22,10 @@ summary(Fumante_CA)
 
 par(cex = 0.7,
     mar = c(2.5,3,2,0.8)+0.1)
+
+colnames(smoke) <- c("Nenhum", "Pouco", "Moderado", "Forte")
+rownames(smoke) <- c("GS", "GJ", "FS", "FJ", "SC")
+
 #------------------------------------------------------------------------------#
 
 
@@ -37,28 +41,29 @@ plot(Fumante_CA)
 
 
 # Variância Explicada Dimensões
-var_exp <- round(Fumante_CA$eig[1:2, 2], 1)
+var_exp <- round(as.numeric(Fumante_CA$eig[1:2, 2]), 1)
 
 
 # Grafico de Correspondência
-grafico <- factoextra::fviz_ca_biplot(Fumante_CA, 
-                           title = "Análise de Correspondência Simples",
-                           labelsize = 5,
-                           pointsize = 2,
-                           arrowsize = 2,
-                           mean.point = TRUE,
-                           graph = TRUE,
-                           repel = TRUE,
-                           label = "all",
-                           shape.row = 19,
-                           shape.col = 17,
-                           alpha.row = "contrib",
-                           map = "symmetric",
-                           axes.linetype = "dotted", # "dashed", "solid"
-                           ggtheme = theme_gray()) +
+grafico <- factoextra::fviz_ca_biplot(
+  Fumante_CA, 
+  title = "Análise de Correspondência Simples", # Título
+  labelsize = 5,                                # Tamanho do Texto
+  pointsize = 2,                                # Tamanho dos Pontos
+  arrowsize = 2,                                # Tamanho das Setas
+  mean.point = TRUE,                            # Ponto médio das observações.
+  graph = TRUE,                                 # Como Gráfico será Desenhado
+  repel = TRUE,                                 # Evita sobreposição de rótulos.
+  label = "all",                                # Legenda para os eixos
+  shape.row = 19,                               # Formato dos Pontos para Linhas
+  shape.col = 17,                               # Formato dos Pontos para Colunas
+  alpha.row = "contrib",                        # Transparência dos pontos
+  map = "symmetric",                            # Tipo de mapeamento usado.
+  axes.linetype = "dotted",                     # Tipo de linha para os eixos
+  ggtheme = theme_gray()) +
   ggplot2::labs(x = paste0("Dimensão I (", var_exp[1], "% Variância Explicada)"),
-                y = paste0("Dimensão II (", var_exp[2], "% Variância Explicada)"))+
-  ggplot2::theme(plot.title = element_text(hjust = 0.5))
+                y = paste0("Dimensão II (", var_exp[2], "% Variância Explicada)")) +
+  ggplot2::theme(plot.title = element_text(hjust = 0.5))  # centraliza o Título
 
   # Exibir Gráfico
   grafico
